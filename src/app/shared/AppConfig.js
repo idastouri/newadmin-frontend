c1aa25fbd0c0e04b956e4a31605762b2d2eabb3e
Alex Svito <asvito@mw2consulting.com>
2017-02-16 23:38:27 +0300

    Return root state

--- src/app/shared/AppConfig.js
+++ src/app/shared/AppConfig.js
@@ -1,13 +1,13 @@
 function AppConfig($stateProvider) {
-  // $stateProvider
-  //   .state('root', {
-  //     abstract: true,
-  //     resolve: {
-  //       // user(UserService) {
-  //       //   return UserService.getCurrentUser();
-  //       // }
-  //     }
-  //   })
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      resolve: {
+        // user(UserService) {
+        //   return UserService.getCurrentUser();
+        // }
+      }
+    })
 }
 
 AppConfig.$inject = ['$stateProvider'];
